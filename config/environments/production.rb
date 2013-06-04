5cd19cfc65201ff2167ec1ee9daaec27e8836db0
Kurt Nelson <kurtisnelson@gmail.com>
2013-06-04 16:50:56 -0400

    More useful logs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,8 @@ Betterlaundry::Application.configure do
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
+  config.logger = Logger.new(STDOUT)
+  config.logger.level = Logger.const_get(ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].upcase : 'INFO')
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
